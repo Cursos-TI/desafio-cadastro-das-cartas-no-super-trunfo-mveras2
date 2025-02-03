d3974065f243912da33b1389a553a145bb255978
mveras2 <202501504353@alunos.estacio.br>
2025-02-03 18:31:59 +0000

    Codigo_Novato-versao_0.1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,6 +17,49 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    
+//_________________________________________________________________________________________________________________________
 
+    //Declaração das variáveis
+    /*para evitar que as variáveis criadas passuam valores indesejados todas as variáveis serão declaradas já com um valor que
+    será substituido durante o programa*/
+    
+    char pais = 'A';
+    int cidade = 0, populacao = 0, pto_tur=0;      
+    float area = 0, PIB =0;            
+
+
+    //Cabeçalho do terminal
+    printf("__________________________________________________________________________________________________________\n");
+    printf("//                              Desafio Super Trunfo - Novato                                           //\n");
+    printf("//           Essa rotina coleta-rá os valores correspondentes de cada uma das cartas.                   //\n");
+    printf("__________________________________________________________________________________________________________\n\n");
+
+    /* Para facilitar a utilização da rotina pelo operador, antes de cada variável será apresentada
+    uma instrução do que se espera coletar*/
+
+    printf("Para a identificação do País utilize uma letra de A-H: \n");
+    scanf("%c", &pais);
+    printf("Para a identificação da cidade utilize uma valor entre 1-8: \n");
+    scanf("%d", &cidade);
+    printf("Inserir o tamanho da população:\n");
+    scanf("%d", &populacao);
+    printf("Inserir o tamanho da área da cidade [km²]:\n");
+    scanf("%f", &area);
+    printf("Inserir o PIB da cidade [R$]:\n");
+    scanf("%f", &PIB);
+    printf("Inserir o número de pontos turísticos da cidade:\n");
+    scanf("%d", &pto_tur);
+
+    //Apresenta os valores inseridos
+
+    printf("\n__________________________________________________________________________________________________________\n\n");
+    printf("Resumo das informações inseridas para cada carta:\n");
+    //printf("País/Cidade: %c"0"%d\n", pais, cidade[]);
+    printf("População: %d\n", populacao);
+    printf("Área [km²]: %f\n", area);
+    printf("PIB: %f\n", PIB);
+    printf("Número de pontos turísticos: %d\n", pto_tur);
+    
     return 0;
 }
